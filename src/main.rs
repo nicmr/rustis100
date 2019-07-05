0ce1a60e94f95cc069b82e14fb275752f5996f66
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-07-05 16:23:34 +0200

    support for multi-digit addition

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use std::fmt::{self, Display};
 use std::error;
 
-
-
-
 fn main() {
-    let sequence = "1+2";
+    let sequence = "14+21";
     println!("Sequence is: {}", sequence);
-    let mut interp = Interpreter::new(sequence);
+    let mut interp = SimpleInterpreter::new(sequence);
     match interp.expr() {
         Ok(result) => println!("result is {}", result),
         Err(e) => println!("Err is {}", e)
     }
 }
-
-
-
 
 #[derive(Copy, Clone, Debug)]
 enum Token {
@@ -36,7 +30,6 @@ enum Token {
 //     Any,
 //     Nil,
 // }
-
 
 
 
@@ -84,40 +77,53 @@ enum TokenErrorKind {
 
 
 // procedural / stateful approach
-struct Interpreter {
+struct SimpleInterpreter {
     text: Vec<char>,
     pos: usize,
 }
 
-impl Interpreter {
-    fn new<S: std::string::ToString>(text: S) -> Interpreter{
-        return Interpreter{
+impl SimpleInterpreter {
+    fn new<S: std::string::ToString>(text: S) -> SimpleInterpreter{
+        return SimpleInterpreter{
             text: text.to_string().chars().collect(),
             pos: 0,
         }
     }
 
     fn get_next_token(&mut self) -> Option<Token> {
+        use std::iter::FromIterator;
+
         // end of input reached
         if self.pos > self.text.len() -1 {
             return Some(Token::EOF);
         }
 
         let current_char = self.text[self.pos];
-        println!("current char is: {}", current_char);
-        self.pos += 1;
+        // println!("first char is: {}", current_char);
+        
 
-        if let Some(i) = current_char.to_digit(10) {
-             // digit
-            // println!("=> identified as digit");
-            Some(Token::Number(i))
+        if current_char.is_digit(10) {
+            
+            let mut length = 1;
+            while self.pos + length < self.text.len() && self.text[self.pos + length].is_digit(10) {
+                length += 1
+            }
+            let s = String::from_iter(self.text[self.pos..(self.pos+length)].iter());
+            // println!("finished token is {}", s);
+            let number = s.parse::<u32>().unwrap();
+            self.pos += length;
+            Some(Token::Number(number))
+
         } else if current_char == '+' {
             // addition operator
             // println!("=> identified as +");
+
+            self.pos += 1;
             Some(Token::Add)
         } else {
             // unknown input
             // println!("=> couldn't identify :(");
+            self.pos += 1;
             None
         }
     }
