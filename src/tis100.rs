279972dc03203712aa4ad2b0107420cff9637668
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-07-07 21:54:39 +0200

    implemented basic structure for running nodes

--- src/tis100.rs
+++ src/tis100.rs
@@ -1,92 +1,76 @@
 use crate::errors::{InterpretError};
 
+pub fn sample_code() -> String {
+    String::from("@0ADD99")
+}
 
 #[derive(Copy, Clone, Debug)]
 enum Token {
     Number(u32),
     NodeID(u32),
     Add,
-    Mov,
     EOF,
+    // To be implemented:
+    // Mov,
+    // Nop,
+    // Swp,
+    // Sav,
+    // Neg,
+    // Jmp,
+    // Jez,
+    // Jnz,
+    // Jlz,
+    // Jro,
 }
 
 
+// IDEA:
+// Don't pass functions for the single instructions, becuase that requires a lot of dyn and Box Code
+// Instead, store Instructions as Enums with operands and their parameters as associated values.
+// Can we achieve an implementation without internal state and mut refs?s
 
+// Goal for now: single node running basic code.
 
+// Issue: instructions can not be mutated by instructions. So instructions should not be parted of mutable data of TisNode.
+// Instead, it may be better to rename the current TisNode type to NodeState, and compose a new type TisNode that contains both
+// the state and the instructions.
 
-
-// struct TisNode {
-//     pub acc: u32,
-//     pub bak: u32,
-//     pub ip: usize, //the instruction pointer / program counter
-
-//     pub code: Vec<Box<TisInstruction>>,
-//     // ports
-//     // storage for last pseudoport
-// }
-
-// impl Clone for TisNode {
-//     fn clone(&self) -> Self {*self}
-// }
-
-// impl TisNode {
-//     pub fn new() -> Self {
-//         TisNode{
-//             acc: 0,
-//             bak: 0,
-//             ip: 0,
-//             code: Vec::new(),
-//         }
-//     }
-//     pub fn tick(&mut self) {
-
-//     }
-// }
-
-// impl std::fmt::Debug for TisNode {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-//         f.debug_struct("TisNode")
-//             .field("acc", &self.acc)
-//             .field("bak", &self.bak)
-//             .field("ip", &self.ip)
-//             .field("bak", &self.bak)
-//             .finish()
-//     }
-// }
-
-// type TisInstruction = Fn(&mut TisNode, Vec::<u32>);
-
-// // TisInstruction
-// fn add(node: &mut TisNode, operands: Vec::<u32>) {
-//     node.acc = node.acc + operands[1];
-// }
-
-// // TisInstruction
-// fn mov(node: &mut TisNode, operands: Vec::<u32>) {
-//     // do fancy stuff
-// }
-
-
-
-
-// #[derive(Copy, Clone, Debug)]
-// enum TisPort{
-//     Up,
-//     Down,
-//     Left,
-//     Right,
-//     Any,
-//     Nil,
-// }
-
-
-
-
-
-
-pub fn sample_code() -> String {
-    String::from("@0ADD99")
+#[derive(Debug, Clone, Copy)]
+pub struct NodeState {
+    pub acc: u32,
+    pub bak: u32,
+    // pub ip: usize, //the instruction pointer / program counter //superfluous?
 }
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum Instruction {
+    Add(u32),
+    Sub(u32),
+    // To be implemented:
+    // Mov,
+    // Nop,
+    // Swp,
+    // Sav,
+    // Neg,
+    // Jmp,
+    // Jez,
+    // Jnz,
+    // Jlz,
+    // Jro,
+}
+
+pub fn tick_n(mut node: NodeState, instructions: Vec<Instruction>, ticks: usize) -> NodeState {
+    let program_length = instructions.len();
+    for tick in 0..ticks {
+        match instructions[tick % program_length] {
+            Instruction::Add(x) => {node.acc += x},
+            Instruction::Sub(x) => {node.acc -= x},
+        }
+    }
+    node
+}
+
 
 
 fn next_token_pure(text: &Vec<char>, position: usize) -> Result<(Token, usize), InterpretError> {
@@ -187,129 +171,3 @@ pub fn expr_pure(text: Vec<char>, position: usize) -> Result<usize, InterpretErr
     }
     Ok((0 + right) as usize)
 }
-
-
-
-// To be deleted:
-// pub struct TisInterpreter {
-//     text: Vec<char>,
-//     pos: usize,
-//     nodes: Vec<TisNode>,
-// }
-// impl TisInterpreter {
-//     pub fn new<S: std::string::ToString>(text: S) -> TisInterpreter{
-//         return TisInterpreter{
-//             text: text.to_string().chars().collect(),
-//             pos: 0,
-//             nodes: Vec::new(),
-//         }
-//     }
-
-
-    
-
-
-//     // should be reworked to return result instead, with a new error type, where
-//     // each error kind represents a single problem encountered while parsing the token 
-//     fn get_next_token(&mut self) -> Option<Token> {
-//         use std::iter::FromIterator;
-
-//         // end of input reached
-//         if self.pos > self.text.len() -1 {
-//             return Some(Token::EOF);
-//         }
-
-//         let current_char = self.text[self.pos];
-//         // println!("first char is: {}", current_char);
-
-
-//         if current_char == '@' {
-//             if self.pos + 1 < self.text.len() && self.text[self.pos + 1].is_digit(10) {
-//                 let id = self.text[self.pos + 1];
-//                 self.pos += 2;
-//                 // we only arrive at this point when id.is_digit(10) was successful, so to_digit(10) will always unwrap successfully
-//                 Some(Token::NodeID(id.to_digit(10).unwrap()))
-//             } else {
-//                 None
-//             }
-//         }
-//         else if current_char.is_digit(10) {
-            
-//             let mut length = 1;
-//             while self.pos + length < self.text.len() && self.text[self.pos + length].is_digit(10) {
-//                 length += 1
-//             }
-//             let s = String::from_iter(self.text[self.pos..(self.pos+length)].iter());
-//             // println!("finished token is {}", s);
-//             let number = s.parse::<u32>().unwrap();
-//             self.pos += length;
-//             Some(Token::Number(number))
-
-//         } else if current_char == 'A' {
-//             // addition operator
-//             // println!("=> identified as +");
-
-//             self.pos += 1;
-//             Some(Token::Add)
-//         } else {
-//             // unknown input
-//             // println!("=> couldn't identify :(");
-//             self.pos += 1;
-//             None
-//         }
-//     }
-
-//     // Parses a single expression in the language.
-//     // Expressions in the TIS-100 language are written in Polish notation
-//     pub fn expr(&mut self) -> Result<u32, TokenError> {
-//         self.nodes = vec![TisNode::new(); 12];
-
-//         let current_node: &mut TisNode;
-//         let mut operator: Box<TisInstruction>;
-//         let mut operands = Vec::new();
-
-//         // 1. A node declaration is required
-//         if let Some(token) = self.get_next_token() {
-//             match token   {
-//                 // TODO: currently supports only nodes 0 through 9
-//                 Token::NodeID(x) => { current_node = &mut self.nodes[x as usize]; },
-//                 _ => { return Err(TokenError::new("Unexpected token: Expected Node declaration")); }
-//             }
-//         } else {
-//             return Err(TokenError::new("Node declaration expected"));
-//         }
-
-//         // 2. An operator is requiired,
-//         // (In polish notation, operators come first, so our 2nd token has to be the operator)
-//         if let Some(token) = self.get_next_token() {
-//             match token {
-//                 Token::Add => {current_node.code.push(Box::new(add))}
-//                 Token::Mov => {current_node.code.push(Box::new(mov))}
-//                 _ => { return Err(TokenError::new("Unexpected Token")) }
-//             }
-//         } else {
-//             return Err(TokenError::new("Unable to get any tokens from text"));
-//         }
-
-
-//         // if let Some(token) = self.get_next_token(){
-//         //     match token {
-//         //         Token::Number(x) => { left = x; },
-//         //         _ => { return Err(TokenError::new("Unexpected Token")) },
-//         //     }
-            
-//         // }
-
-//         // 2nd and 3rd must be a number each
-//         'a: while let Some(token) = self.get_next_token(){
-//             match token {
-//                 Token::Number(x) => { operands.push(x) },
-//                 _ => { return Err(TokenError::new("Unexpected Token")) }
-//             }
-//         }
-
-//         operator(&mut self.nodes[0], operands);
-//         Ok(self.nodes[0].acc)
-//     }
-// }
-
